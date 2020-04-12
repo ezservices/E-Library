4416a5decb520ddf5d63fd7408915bed1b788052
ezservices <ezservices1920@gmail.com>
2020-04-12 11:48:25 +0530

    Books routing

--- app.js
+++ app.js
@@ -12,12 +12,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
+const books = [
+    {
+        title:"Book1",
+        description:"ome quick example text to build on the card title and make up the bulk of the card's content."
+    },
+    {
+        title:"Book2",
+        description:"ome quick example text to build on the card title and make up the bulk of the card's content."
+    },
+    {
+        title:"Book3",
+        description:"ome quick example text to build on the card title and make up the bulk of the card's content."
+    },
+
+]
+
+
 app.get("/",function(req,res){
-    res.render("elibrary")
+    res.redirect("/elibrary");
+})
+
+app.get("/elibrary",function(req,res){
+    res.render("elibrary",{books:books});
 })
 
 
+app.get("/elibrary/:book",function (req,res) {
 
+    const title = req.params.book;
+    res.render("show",{title:title})
+    
+})
 
 
 
